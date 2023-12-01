4be4d83b809029e2b6e1c4ab4f3bc206785f5985
KMSBismarck1402 <mtrietvu@gmail.com>
2023-12-02 01:05:33 +0700

    a

--- python/hs3/call_linear_function.py
+++ python/hs3/call_linear_function.py
@@ -6,14 +6,15 @@ import pandas as pd
 
 def main():
     # Replace 'your_data.csv' with the actual path to your CSV file
-    file_path = 'Housing_data_final(2).csv'
+    file_path = 'Housing_data_final(4).csv'
 
     # Replace with the features you want to use
     num_vars = ['area', 'bedrooms', 'bathrooms', 'washingmachine', 'dtcenter',
                 'dtrec', 'dthospital', 'hotwater', 'ac', 'furnish', 'parking', 'security']
 
     # Train the model and save it (uncomment the following line if you haven't trained the model yet)
-    # model, poly = train_polynomial_regression(file_path, num_vars, save_model_path='trained_model.pkl')
+    model, poly = train_polynomial_regression(
+        file_path, num_vars, save_model_path='trained_model.pkl')
 
     # Load the trained model and polynomial features
     model, poly = load_trained_model(
@@ -21,13 +22,13 @@ def main():
 
     # User input for feature values
     vari = int(0)
-    value = [110, 1, 1, 1, 0.1, 0.2, 0.3, 1, 1, 2, 1, 1]
+    value = [70, 1, 1, 1, 0.7, 1, 0.5, 1, 1, 2, 1, 1]
 
     user_input = {}
     for var in num_vars:
         # value = float(input(f"Enter the value for {var}: "))
         user_input[var] = value[vari]
-        vari = vari+1 
+        vari = vari+1
 
     # Create a DataFrame for user input
     user_df = pd.DataFrame([user_input])
